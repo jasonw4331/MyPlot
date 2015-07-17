f56e1835edf5640164edd846acc1a9d3e9db3c1c
Blumieare Exxarion <Apluscomics@live.com>
2015-07-17 17:10:10 -0400

    Added "removehelper" subcommand
    Added the ability to remove helpers

---
+++ src/MyPlot/subcommand/RemoveHelperSubCommand.php
@@ -0,0 +1,64 @@
+<?php
+namespace MyPlot\subcommand;
+
+use MyPlot\MyPlot;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class RemoveHelperSubCommand implements SubCommand
+{
+    private $plugin;
+
+    public function __construct(MyPlot $plugin) {
+        $this->plugin = $plugin;
+    }
+
+    public function canUse(CommandSender $sender) {
+        return ($sender instanceof Player);
+    }
+
+    public function getUsage() {
+        return "<player>";
+    }
+
+    public function getName() {
+        return "removehelper";
+    }
+
+    public function getDescription() {
+        return "Remove a helper from your plot";
+    }
+
+    public function getAliases() {
+        return ["delh"];
+    }
+
+    public function execute(CommandSender $sender, array $args) {
+        if (count($args) !== 1) {
+            return false;
+        }
+        $helper = $args[0];
+        $player = $sender->getServer()->getPlayer($sender->getName());
+        $plot = $this->plugin->getPlotByPosition($player->getPosition());
+        if ($plot === null) {
+            $sender->sendMessage(TextFormat::RED . "You are not standing inside a plot");
+            return true;
+        }
+        if ($plot->owner !== $sender->getName()) {
+            $sender->sendMessage(TextFormat::RED . "You are not the owner of this plot");
+            return true;
+        }
+        if (!$plot->removeHelper($helper)) {
+            $sender->sendMessage($helper . " was never a helper of this plot.");
+            return true;
+        }
+        var_dump($plot);
+        if ($this->plugin->getProvider()->savePlot($plot)) {
+            $sender->sendMessage(TextFormat::GREEN . $helper . " has been removed.");
+        } else {
+            $sender->sendMessage(TextFormat::RED . "Could not remove that player.");
+        }
+        return true;
+    }
+}
