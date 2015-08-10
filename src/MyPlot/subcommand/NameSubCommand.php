3d6d9e772c3d5e14e0e6eec8a0d21bebc667e853
Blumieare Exxarion <Apluscomics@live.com>
2015-08-10 15:36:19 -0400

    Create NameSubCommand.php

---
+++ src/MyPlot/subcommand/NameSubCommand.php
@@ -0,0 +1,55 @@
+<?php
+namespace MyPlot\subcommand;
+
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class NameSubCommand extends SubCommand
+{
+    public function canUse(CommandSender $sender) {
+        return ($sender instanceof Player) and $sender->hasPermission("myplot.command.name");
+    }
+
+    public function getUsage() {
+        return "<name>";
+    }
+
+    public function getName() {
+        return "name";
+    }
+
+    public function getDescription() {
+        return "Names the plot that you are standing on";
+    }
+
+    public function getAliases() {
+        return [];
+    }
+
+    public function execute(CommandSender $sender, array $args) {
+        if (count($args) !== 1) {
+            return false;
+        }
+        
+        $player = $sender->getServer()->getPlayer($sender->getName());
+        $plot = $this->getPlugin()->getPlotByPosition($player->getPosition());
+        if ($plot === null) {
+            $sender->sendMessage(TextFormat::RED . "You are not standing inside a plot");
+            return true;
+        }
+        if ($plot->owner !== $sender->getName() and !$sender->hasPermission("myplot.admin.name")) {
+            $sender->sendMessage(TextFormat::RED . "You are not the owner of this plot");
+            return true;
+        }
+        
+        $name = $args[0];
+        $plot->name = $name;
+        if ($this->getPlugin()->getProvider()->savePlot($plot)) {
+            $sender->sendMessage(TextFormat::GREEN . "Changed the plot name to " . TextFormat::WHITE . $plot . TextFormat::GREEN ." Successfully!");
+        } else {
+            $sender->sendMessage(TextFormat::RED . "Could not change the name.");
+        }
+        return true;
+    }
+}
