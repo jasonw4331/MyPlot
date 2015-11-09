67b972390558c9ba3b609252234877d71cee22b5
wiez <wies.luyten@gmail.com>
2015-11-09 17:48:52 +0100

    Fixed bug reduse money by zero

--- src/MyPlot/provider/EconomySProvider.php
+++ src/MyPlot/provider/EconomySProvider.php
@@ -7,15 +7,14 @@ use pocketmine\Player;
 class EconomySProvider implements EconomyProvider
 {
     public function reduceMoney(Player $player, $amount) {
-        if ($amount < 0) {
+        if ($amount == 0) {
+            return true;
+        } elseif ($amount < 0) {
             $ret = EconomyAPI::getInstance()->addMoney($player, -$amount, true);
         } else {
             $ret = EconomyAPI::getInstance()->reduceMoney($player, $amount, true);
         }
-        if ($ret === EconomyAPI::RET_SUCCESS) {
-            return true;
-        } else {
-            return false;
-        }
+
+        return ($ret === EconomyAPI::RET_SUCCESS);
     }
 }
