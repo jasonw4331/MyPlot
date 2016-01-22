6ccdded1561e3cde3abd49e1157147ef52fa09a5
wiez <wies.luyten@gmail.com>
2016-01-22 16:35:11 +0100

    Improved help command; Commands split in pages

--- src/MyPlot/subcommand/HelpSubCommand.php
+++ src/MyPlot/subcommand/HelpSubCommand.php
@@ -2,6 +2,7 @@
 namespace MyPlot\subcommand;
 
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\utils\TextFormat;
 
 class HelpSubCommand extends SubCommand
@@ -19,11 +20,37 @@ class HelpSubCommand extends SubCommand
     }
 
     public function execute(CommandSender $sender, array $args) {
-        $sender->sendMessage($this->translateString("help.header"));
+        if (count($args) === 0) {
+            $pageNumber = 1;
+        } elseif (is_numeric($args[0])) {
+            $pageNumber = (int) array_shift($args);
+            if ($pageNumber <= 0) {
+                $pageNumber = 1;
+            }
+        } else {
+            return false;
+        }
+
+        if ($sender instanceof ConsoleCommandSender) {
+            $pageHeight = PHP_INT_MAX;
+        } else {
+            $pageHeight = 5;
+        }
+
+        $commands = [];
         foreach ($this->getCommandHandler()->getCommands() as $command) {
             if ($command->canUse($sender)) {
-                $sender->sendMessage(TextFormat::DARK_GREEN . $command->getName() . ": " . TextFormat::WHITE . $command->getDescription());
+                $commands[$command->getName()] = $command;
             }
+        }
+        ksort($commands, SORT_NATURAL | SORT_FLAG_CASE);
+        $commands = array_chunk($commands, $pageHeight);
+        /** @var SubCommand[][] $commands */
+        $pageNumber = (int) min(count($commands), $pageNumber);
+
+        $sender->sendMessage($this->translateString("help.header", [$pageNumber, count($commands)]));
+        foreach ($commands[$pageNumber - 1] as $command) {
+            $sender->sendMessage(TextFormat::DARK_GREEN . $command->getName() . ": " . TextFormat::WHITE . $command->getDescription());
         }
         return true;
     }
