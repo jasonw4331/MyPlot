3d67956a0d9ffa35863d63ffd3131ecc4d3306cb
jasonwynn10 <jasonwynn10@gmail.com>
2016-09-20 14:24:59 -0400

    Will finish later

--- src/MyPlot/subcommand/HelpSubCommand.php
+++ src/MyPlot/subcommand/HelpSubCommand.php
@@ -14,41 +14,12 @@ class HelpSubCommand extends SubCommand
     }
 
     public function execute(CommandSender $sender, array $args) {
-        if (count($args) === 0) {
-            $pageNumber = 1;
-        } elseif (is_numeric($args[0])) {
-            $pageNumber = (int) array_shift($args);
-            if ($pageNumber <= 0) {
-                $pageNumber = 1;
-            }
-        } else {
-            return false;
+        if($args[0]) {
+            
         }
-
-        if ($sender instanceof ConsoleCommandSender) {
-            $pageHeight = PHP_INT_MAX;
-        } else {
-            $pageHeight = 5;
-        }
-
-        $commands = [];
-        foreach (MyPlot::getInstance()->getDescription()->getCommands() as $command) {
-            if($command instanceof Command);
-            foreach($sender->getEffectivePermissions() as $permission) {
-                if($command->getPermission() === $permission->getName()) {
-                    $commands[$command->getName()] = $command;
-                }
-            }
-        }
-        ksort($commands, SORT_NATURAL | SORT_FLAG_CASE);
-        $commands = array_chunk($commands, $pageHeight);
-        /** @var SubCommand[][] $commands */
-        $pageNumber = (int) min(count($commands), $pageNumber);
-
-        $sender->sendMessage($this->translateString("help.header", [$pageNumber, count($commands)]));
-        foreach ($commands[$pageNumber - 1] as $command) {
-            $sender->sendMessage(TextFormat::DARK_GREEN . $command->getName() . ": " . TextFormat::WHITE . $command->getDescription());
-        }
+        $sender->sendMessage(TextFormat::$this->translateString("help.header", [$pageNumber, 5]));
+        $sender->sendMessage(TextFormat::DARK_GREEN.": ".TextFormat::WHITE."");
+        
         return true;
     }
 }
