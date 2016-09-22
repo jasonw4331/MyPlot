6afc9852d9b2d81af8b53d9197832ed9750dfe3d
jasonwynn10 <jasonwynn10@gmail.com>
2016-09-22 09:26:00 -0400

    Create SetOwnerSubCommand.php
    I think this will work to reset the plots owner and the plots name

---
+++ src/MyPlot/subcommand/SetOwnerSubCommand.php
@@ -0,0 +1,38 @@
+<?php
+namespace MyPlot\subcommand;
+
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class SetOwnerSubCommand extends SubCommand {
+    public function canUse(CommandSender $sender) {
+        return ($sender instanceof Player) and $sender->hasPermission("myplot.admin.setowner");
+    }
+    public function execute(CommandSender $sender, array $args) {
+        if (count($args) < 1) {
+            return false;
+        }
+        $player = $sender->getServer()->getPlayer($sender->getName());
+        $plot = $this->getPlugin()->getPlotByPosition($player->getPosition());
+        if ($plot === null) {
+            $sender->sendMessage(TextFormat::RED . $this->translateString("notinplot"));
+            return true;
+        }
+        $maxPlots = $this->getPlugin()->getMaxPlotsOfPlayer($player);
+        $plotsOfPlayer = count($this->getPlugin()->getProvider()->getPlotsByOwner($player->getName()));
+        if ($plotsOfPlayer >= $maxPlots) {
+            $sender->sendMessage(TextFormat::RED . $this->translateString("setowner.maxplots", [$maxPlots]));
+            return true;
+        }
+        $plotLevel = $this->getPlugin()->getLevelSettings($plot->levelName);
+        $plot->owner = $args[0];
+        $plot->name = "";
+        if ($this->getPlugin()->getProvider()->savePlot($plot)) {
+            $sender->sendMessage($this->translateString("setowner.success", $plot->owner));
+        } else {
+            $sender->sendMessage(TextFormat::RED . $this->translateString("error"));
+        }
+        return true;
+    }
+}
