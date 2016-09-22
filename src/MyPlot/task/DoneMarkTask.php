9df8d488b269a368599f9fbd85a6a0cb2209a8a7
jasonwynn10 <jasonwynn10@gmail.com>
2016-09-22 11:22:33 -0400

    Update DoneMarkTask.php
    Added informations floating text

--- src/MyPlot/task/DoneMarkTask.php
+++ src/MyPlot/task/DoneMarkTask.php
@@ -1,11 +1,14 @@
 <?php
 namespace MyPlot\task;
-use MyPlot\MyPlot;
-use MyPlot\Plot;
+
 use pocketmine\level\particle\HappyVillagerParticle;
 use pocketmine\scheduler\PluginTask;
-class DoneMarkTask extends PluginTask
-{
+use pocketmine\utils\TextFormat as TF;
+
+use MyPlot\MyPlot;
+use MyPlot\Plot;
+
+class DoneMarkTask extends PluginTask {
     private $done, $plot, $plugin;
     public function __construct(MyPlot $plugin, Plot $plot) {
         parent::__construct($plugin);
@@ -17,5 +20,19 @@ class DoneMarkTask extends PluginTask
         $lname = $this->plot->levelName;
         $plocation = $this->plugin->getPlotMid($this->plot);
         $this->plugin->getServer()->getLevel($lname)->addParticle(new HappyVillagerParticle($plocation));
+        $text = [];
+        
+        $br = TF::RESET. "\n";
+        
+        $text[0] = TF::BLUE. F::BOLD. $this->plot->name;
+        $text[1] = TF::DARK_GREEN."By: ".$this->plot->owner;
+       
+        $level = $this->getServer()->getLevel($lname);
+       
+        $title = TF::RESET. $text[0]. TF::RESET;
+        $texter = $text[1];
+        $particle = new FloatingTextParticle(new Vector3($plocation->x, $plocation->y+5, $plocation->z), $texter, $title);
+
+        $level->addParticle($particle, $p);
     }
 }
