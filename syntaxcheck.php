ce2b3d5b2374ff027020971501d5d6d67facf493
jasonwynn10 <jasonwynn10@gmail.com>
2016-11-16 13:17:12 -0500

    Create syntaxcheck.php

---
+++ syntaxcheck.php
@@ -0,0 +1,12 @@
+<?php
+$base = glob("plugins/MyPlot-src/src/*.php");
+$providers = glob("plugins/MyPlot-src/src/provider/*.php");
+$commands = glob("plugins/MyPlot-src/src/subcommand/*.php");
+$tasks = glob("plugins/MyPlot-src/src/Tasks/*.php");
+$files = array_merge($base, $providers, $commands, $tasks);
+foreach($files as $file){
+    exec("php -l $file", $output);
+}
+foreach($output as $line){
+    echo($line);
+}
