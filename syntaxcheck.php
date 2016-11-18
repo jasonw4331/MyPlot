8a43998356a8e276d6097813ac8aeb6de3bdf630
jasonwynn10 <jasonwynn10@gmail.com>
2016-11-18 08:48:34 -0500

    Update syntaxcheck.php

--- syntaxcheck.php
+++ syntaxcheck.php
@@ -4,6 +4,7 @@ $providers = glob("plugins/MyPlot-src/src/provider/*.php");
 $commands = glob("plugins/MyPlot-src/src/subcommand/*.php");
 $tasks = glob("plugins/MyPlot-src/src/Tasks/*.php");
 $files = array_merge($base, $providers, $commands, $tasks);
+$output = [];
 foreach($files as $file){
     exec("php -l $file", $output);
 }
