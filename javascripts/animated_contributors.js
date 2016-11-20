e8f9cf822b6872247ac95e9a722baed4ca95f4d5
jasonwynn10 <jasonwynn10@gmail.com>
2016-11-19 21:16:18 -0500

    Added more things to edit later

---
+++ javascripts/animated_contributors.js
@@ -0,0 +1,16 @@
+var myName = "Contributors";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue, purple];
+drawName(myName, letterColors);
+if (10 < 3) {
+    bubbleShape = 'square';
+}
+else {
+    bubbleShape = 'circle';
+}
+bounceBubbles();
