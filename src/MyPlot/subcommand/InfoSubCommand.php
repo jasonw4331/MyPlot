c7b7c1da1ba44df354b1ed2e1eba1ebbb4df1b0e
jasonwynn10 <jasonwynn10@gmail.com>
2018-01-22 09:56:44 -0500

    Started work on the feature mentioned in #113

--- src/MyPlot/subcommand/InfoSubCommand.php
+++ src/MyPlot/subcommand/InfoSubCommand.php
@@ -21,19 +21,44 @@ class InfoSubCommand extends SubCommand
 	 * @return bool
 	 */
 	public function execute(CommandSender $sender, array $args) {
-		$plot = $this->getPlugin()->getPlotByPosition($sender->getPosition());
-		if ($plot === null) {
-			$sender->sendMessage(TextFormat::RED . $this->translateString("notinplot"));
-			return true;
+		if(isset($args[0])) {
+			if(isset($args[1]) and is_numeric($args[1])) {
+				$key = (int) $args[1];
+				$plots = [];
+				foreach ($this->getPlugin()->getPlotLevels() as $levelName => $settings) {
+					$plots = array_merge($plots, $this->getPlugin()->getPlotsOfPlayer($args[0], $levelName));
+				}
+				if(isset($plots[$key])) {
+					$plot = $plots[$key];
+					$sender->sendMessage($this->translateString("info.about", [TextFormat::GREEN . $plot]));
+					$sender->sendMessage($this->translateString("info.owner", [TextFormat::GREEN . $plot->owner]));
+					$sender->sendMessage($this->translateString("info.plotname", [TextFormat::GREEN . $plot->name]));
+					$helpers = implode(", ", $plot->helpers);
+					$sender->sendMessage($this->translateString("info.helpers", [TextFormat::GREEN . $helpers]));
+					$denied = implode(", ", $plot->denied);
+					$sender->sendMessage($this->translateString("info.denied", [TextFormat::GREEN . $denied]));
+					$sender->sendMessage($this->translateString("info.biome", [TextFormat::GREEN . $plot->biome]));
+				}else{
+					$sender->sendMessage(TextFormat::RED . $this->translateString("info.notfound"));
+				}
+			}else{
+				return false;
+			}
+		}else{
+			$plot = $this->getPlugin()->getPlotByPosition($sender);
+			if ($plot === null) {
+				$sender->sendMessage(TextFormat::RED . $this->translateString("notinplot"));
+				return true;
+			}
+			$sender->sendMessage($this->translateString("info.about", [TextFormat::GREEN . $plot]));
+			$sender->sendMessage($this->translateString("info.owner", [TextFormat::GREEN . $plot->owner]));
+			$sender->sendMessage($this->translateString("info.plotname", [TextFormat::GREEN . $plot->name]));
+			$helpers = implode(", ", $plot->helpers);
+			$sender->sendMessage($this->translateString("info.helpers", [TextFormat::GREEN . $helpers]));
+			$denied = implode(", ", $plot->denied);
+			$sender->sendMessage($this->translateString("info.denied", [TextFormat::GREEN . $denied]));
+			$sender->sendMessage($this->translateString("info.biome", [TextFormat::GREEN . $plot->biome]));
 		}
-		$sender->sendMessage($this->translateString("info.about", [TextFormat::GREEN . $plot]));
-		$sender->sendMessage($this->translateString("info.owner", [TextFormat::GREEN . $plot->owner]));
-		$sender->sendMessage($this->translateString("info.plotname", [TextFormat::GREEN . $plot->name]));
-		$helpers = implode(", ", $plot->helpers);
-		$sender->sendMessage($this->translateString("info.helpers", [TextFormat::GREEN . $helpers]));
-		$denied = implode(", ", $plot->denied);
-		$sender->sendMessage($this->translateString("info.denied", [TextFormat::GREEN . $denied]));
-		$sender->sendMessage($this->translateString("info.biome", [TextFormat::GREEN . $plot->biome]));
 		return true;
 	}
 }
