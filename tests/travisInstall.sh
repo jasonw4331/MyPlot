c67a0c3b5e5dc23a4288d631efeda528a60d4010
jasonwynn10 <jasonwynn10@gmail.com>
2018-02-14 15:52:30 -0500

    move everything to travis config

--- tests/travisInstall.sh
+++ tests/travisInstall.sh
@@ -10,7 +10,7 @@ PM_DL_PATH="${1:-"https://github.com/pmmp/PocketMine-MP/releases/download/api%2F
 mkdir "$TRAVIS_BUILD_DIR"/../PocketMine && cd "$TRAVIS_BUILD_DIR"/../PocketMine
 echo "Installing PocketMine in $PWD"
 echo "Downloading PocketMine build from Poggit"
-wget -O PocketMine-MP.phar "$PM_DL_PATH"
+wget -O PocketMine-MP.phar "${1:-"https://github.com/pmmp/PocketMine-MP/releases/download/api%2F3.0.0-ALPHA11/PocketMine-MP_1.7dev-677_07bf1c9e_API-3.0.0-ALPHA11.phar"}"
 mkdir plugins && wget -O plugins/PluginChecker.phar https://poggit.pmmp.io/res/PluginChecker.phar
 
 echo "Downloading Poggit build"
