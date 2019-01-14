ebf202c9d8269edc14c44eda153cc86887f17304
jasonwynn10 <jasonwynn10@me.com>
2019-01-14 13:14:20 -0500

    Improved tile removal

--- src/MyPlot/task/ClearPlotTask.php
+++ src/MyPlot/task/ClearPlotTask.php
@@ -82,7 +82,7 @@ class ClearPlotTask extends Task {
 		}
 		foreach($this->level->getTiles() as $tile) {
 			if(($plot = $this->plugin->getPlotByPosition($tile)) != null) {
-				if($plot->X === $this->plotBeginPos->x and $plot->Z === $this->plotBeginPos->z) {
+				if($plot->X === $this->plot->X and $plot->Z === $this->plot->Z) {
 					$tile->close();
 				}
 			}
