2307eadfe857c4aafd9ebec2001c36c9e88cf8eb
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2019-08-02 19:23:22 +0200

    Update AddHelperSubCommand.php

--- src/MyPlot/subcommand/AddHelperSubCommand.php
+++ src/MyPlot/subcommand/AddHelperSubCommand.php
@@ -1,57 +1,53 @@
 <?php
 declare(strict_types=1);
 namespace MyPlot\subcommand;
-
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
-use pocketmine\OfflinePlayer;
 use pocketmine\utils\TextFormat;
-
 class AddHelperSubCommand extends SubCommand
 {
-	/**
-	 * @param CommandSender $sender
-	 *
-	 * @return bool
-	 */
-	public function canUse(CommandSender $sender) : bool {
-		return ($sender instanceof Player) and $sender->hasPermission("myplot.command.addhelper");
-	}
-
-	/**
-	 * @param Player $sender
-	 * @param string[] $args
-	 *
-	 * @return bool
-	 */
-	public function execute(CommandSender $sender, array $args) : bool {
-		if(empty($args)) {
-			return false;
-		}
-		$helper = $args[0];
-		$plot = $this->getPlugin()->getPlotByPosition($sender);
-		if($plot === null) {
-			$sender->sendMessage(TextFormat::RED . $this->translateString("notinplot"));
-			return true;
-		}
-		if($plot->owner !== $sender->getName() and !$sender->hasPermission("myplot.admin.addhelper")) {
-			$sender->sendMessage(TextFormat::RED . $this->translateString("notowner"));
-			return true;
-		}
-		$name = $helper;
-		$helper = $this->getPlugin()->getServer()->getPlayer($helper) ?? $this->getPlugin()->getServer()->getOfflinePlayer($helper);
-		$function = new function($player, $name){
-			if($player instanceof Player){
-				return $player->getName();
-			}elseif($player instanceof OfflinePlayer){
-				return $name;
-			}
-		};
+    /**
+     * @param CommandSender $sender
+     *
+     * @return bool
+     */
+    public function canUse(CommandSender $sender) : bool {
+        return ($sender instanceof Player) and $sender->hasPermission("myplot.command.addhelper");
+    }
+    /**
+     * @param Player $sender
+     * @param string[] $args
+     *
+     * @return bool
+     */
+    public function execute(CommandSender $sender, array $args) : bool {
+        if(empty($args)) {
+            return false;
+        }
+        $helper = $args[0];
+        $plot = $this->getPlugin()->getPlotByPosition($sender);
+        if($plot === null) {
+            $sender->sendMessage(TextFormat::RED . $this->translateString("notinplot"));
+            return true;
+        }
+        if($plot->owner !== $sender->getName() and !$sender->hasPermission("myplot.admin.addhelper")) {
+            $sender->sendMessage(TextFormat::RED . $this->translateString("notowner"));
+            return true;
+        }
+        $name = $helper;
+        $helper = $this->getPlugin()->getServer()->getPlayer($helper) ?? $this->getPlugin()->getServer()->getOfflinePlayer($helper);
+        $function = function($player, $name) {
+            if ($player instanceof Player) {
+                return $player->getName();
+            } else {
+                return $name;
+            }
+        };
 		if($this->getPlugin()->addPlotHelper($plot, $function($helper, $name))) {
-			$sender->sendMessage($this->translateString("addhelper.success", [$helper->getName()]));
-		}else{
-			$sender->sendMessage(TextFormat::RED . $this->translateString("error"));
-		}
+            $sender->sendMessage($this->translateString("addhelper.success", [$helper->getName()]));
+        }else{
+            $sender->sendMessage(TextFormat::RED . $this->translateString("error"));
+        }
 		return true;
 	}
 }
