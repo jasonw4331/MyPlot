3445f77baa0031da57700bf252f1d01f504dc5f3
jasonwynn10 <jasonwynn10@me.com>
2019-12-15 13:29:34 -0500

    Download PhpStan and PhpUnit

--- tests/travisScript.sh
+++ tests/travisScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-[ ! -f phpstan.phar ] && echo "Downloading PHPStan..." && curl -sSLO https://api.github.com/repos/phpstan/phpstan/releases/latest \| grep "browser_download_url.*phar" \| cut -d : -f 2,3 \| tr -d \" \| wget -qi -
+echo "Downloading PHPStan..." && curl -sSLO https://api.github.com/repos/phpstan/phpstan/releases/latest \| grep "browser_download_url.*phar" \| cut -d : -f 2,3 \| tr -d \" \| wget -qi -
 php phpstan.phar analyze --no-progress --memory-limit=2G || exit 1
 echo "PHPStan scan succeeded"
 
-[ ! -f phpunit.phar ] && echo "Downloading PHPUnit..." && curl https://phar.phpunit.de/phpunit-7.phar --silent --location -o phpunit.phar
-"$PHP_BINARY" phpunit.phar --bootstrap vendor/autoload.php --fail-on-warning tests/phpunit || exit 1
+echo "Downloading PHPUnit..." && curl https://phar.phpunit.de/phpunit-7.phar --silent --location -o phpunit.phar
+php phpunit.phar --bootstrap vendor/autoload.php --fail-on-warning tests/phpunit || exit 1
 
 if [ "$1" == "" ]; then
     echo Usage: travisScript.sh '<name of plugin to be tested> <name of project to be tested>'
