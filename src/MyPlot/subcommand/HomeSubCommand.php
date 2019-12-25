5e1cce9656b32e92997a0341956c13d53f9063e7
Crasher508 <45127993+Crasher508@users.noreply.github.com>
2019-12-25 18:56:08 +0100

    Update HomeSubCommand.php
    With this Changes Player can use "/p h <playername> <plotnumber>"

--- src/MyPlot/subcommand/HomeSubCommand.php
+++ src/MyPlot/subcommand/HomeSubCommand.php
@@ -9,51 +9,124 @@ use pocketmine\utils\TextFormat;
 
 class HomeSubCommand extends SubCommand
 {
-	/**
-	 * @param CommandSender $sender
-	 *
-	 * @return bool
-	 */
-	public function canUse(CommandSender $sender) : bool {
-		return ($sender instanceof Player) and $sender->hasPermission("myplot.command.home");
-	}
+    /**
+     * @param CommandSender $sender
+     *
+     * @return bool
+     */
+    public function canUse(CommandSender $sender): bool
+    {
+        return ($sender instanceof Player) and $sender->hasPermission("myplot.command.home");
+    }
 
-	/**
-	 * @param Player $sender
-	 * @param string[] $args
-	 *
-	 * @return bool
-	 */
-	public function execute(CommandSender $sender, array $args) : bool {
-		if(empty($args)) {
-			$plotNumber = 1;
-		}elseif(is_numeric($args[0])) {
-			$plotNumber = (int) $args[0];
-		}else{
-			return false;
-		}
-		$levelName = $args[1] ?? $sender->getLevel()->getFolderName();
-		$plots = $this->getPlugin()->getPlotsOfPlayer($sender->getName(), $levelName);
-		if(empty($plots)) {
-			$sender->sendMessage(TextFormat::RED . $this->translateString("home.noplots"));
-			return true;
-		}
-		if(!isset($plots[$plotNumber - 1])) {
-			$sender->sendMessage(TextFormat::RED . $this->translateString("home.notexist", [$plotNumber]));
-			return true;
-		}
-		usort($plots, function(Plot $plot1, Plot $plot2) {
-			if($plot1->levelName == $plot2->levelName) {
-				return 0;
-			}
-			return ($plot1->levelName < $plot2->levelName) ? -1 : 1;
-		});
-		$plot = $plots[$plotNumber - 1];
-		if($this->getPlugin()->teleportPlayerToPlot($sender, $plot)) {
-			$sender->sendMessage($this->translateString("home.success", [$plot, $plot->levelName]));
-		}else{
-			$sender->sendMessage(TextFormat::RED . $this->translateString("home.error"));
-		}
-		return true;
-	}
+    /**
+     * @param Player $sender
+     * @param string[] $args
+     *
+     * @return bool
+     */
+    public function execute(CommandSender $sender, array $args): bool
+    {
+
+        if (isset($args[0]) && is_numeric($args[0]) === false) {
+            if (!$s = $this->getPlugin()->getServer()->getPlayer($args[0])) {
+                $p = $args[0];
+            } else {
+                $p = $s->getName();
+            }
+            if (isset($args[1]) && is_numeric($args[1])) {
+                $plotNumber = (int)$args[1];
+            } else {
+                $plotNumber = (int)1;
+            }
+            $levelName = $sender->getLevel()->getFolderName();
+            $plots = $this->getPlugin()->getPlotsOfPlayer($p, $levelName);
+            if (empty($plots)) {
+                $sender->sendMessage(TextFormat::RED . $this->translateString("home.noplots"));
+                return true;
+            }
+            if (!isset($plots[$plotNumber - 1])) {
+                $sender->sendMessage(TextFormat::RED . "The Player have no plot with ID: " . $plotNumber . " in this world.");
+                return true;
+            }
+            usort($plots, function (Plot $plot1, Plot $plot2) {
+                if ($plot1->levelName == $plot2->levelName) {
+                    return 0;
+                }
+                return ($plot1->levelName < $plot2->levelName) ? -1 : 1;
+            });
+            $plot = $plots[$plotNumber - 1];
+            if ($this->getPlugin()->teleportPlayerToPlot($sender, $plot)) {
+                $sender->sendMessage("Teleport to plot " . $plot . " from " . $p);
+            } else {
+                $sender->sendMessage(TextFormat::RED . $this->translateString("home.error"));
+            }
+
+        } else if (!isset($args[0])) {
+            $p = $sender->getName();
+
+            if (isset($args[0]) && is_numeric($args[0])) {
+                $plotNumber = (int)$args[0];
+            } else {
+                $plotNumber = (int)1;
+            }
+
+            $levelName = $sender->getLevel()->getFolderName();
+            $plots = $this->getPlugin()->getPlotsOfPlayer($p, $levelName);
+            if (empty($plots)) {
+                $sender->sendMessage(TextFormat::RED . $this->translateString("home.noplots"));
+                return true;
+            }
+            if (!isset($plots[$plotNumber - 1])) {
+                $sender->sendMessage(TextFormat::RED . $this->translateString("home.notexist", [$plotNumber]));
+                return true;
+            }
+            usort($plots, function (Plot $plot1, Plot $plot2) {
+                if ($plot1->levelName == $plot2->levelName) {
+                    return 0;
+                }
+                return ($plot1->levelName < $plot2->levelName) ? -1 : 1;
+            });
+            $plot = $plots[$plotNumber - 1];
+            if ($this->getPlugin()->teleportPlayerToPlot($sender, $plot)) {
+                $sender->sendMessage($this->translateString("home.success", [$plot, $plot->levelName]));
+            } else {
+                $sender->sendMessage(TextFormat::RED . $this->translateString("home.error"));
+            }
+        } else if (isset($args[0]) && is_numeric($args[0])) {
+            $p = $sender->getName();
+
+            if (isset($args[0]) && is_numeric($args[0])) {
+                $plotNumber = (int)$args[0];
+            } else {
+                $plotNumber = (int)1;
+            }
+
+            $levelName = $sender->getLevel()->getFolderName();
+            $plots = $this->getPlugin()->getPlotsOfPlayer($p, $levelName);
+            if (empty($plots)) {
+                $sender->sendMessage(TextFormat::RED . $this->translateString("home.noplots"));
+                return true;
+            }
+            if (!isset($plots[$plotNumber - 1])) {
+                $sender->sendMessage(TextFormat::RED . $this->translateString("home.notexist", [$plotNumber]));
+                return true;
+            }
+            usort($plots, function (Plot $plot1, Plot $plot2) {
+                if ($plot1->levelName == $plot2->levelName) {
+                    return 0;
+                }
+                return ($plot1->levelName < $plot2->levelName) ? -1 : 1;
+            });
+            $plot = $plots[$plotNumber - 1];
+            if ($this->getPlugin()->teleportPlayerToPlot($sender, $plot)) {
+                $sender->sendMessage($this->translateString("home.success", [$plot, $plot->levelName]));
+            } else {
+                $sender->sendMessage(TextFormat::RED . $this->translateString("home.error"));
+            }
+        }
+
+
+        return true;
+    }
 }
