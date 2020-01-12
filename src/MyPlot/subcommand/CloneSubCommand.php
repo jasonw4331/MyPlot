f89d7b058635b03ac46008944050505d776158cd
jasonwynn10 <jasonwynn10@me.com>
2020-01-12 12:08:15 -0500

    Make clone subcommand less confusing

--- src/MyPlot/subcommand/CloneSubCommand.php
+++ src/MyPlot/subcommand/CloneSubCommand.php
@@ -34,28 +34,28 @@ class CloneSubCommand extends SubCommand
 			return true;
 		}
 		$levelName = $args[1] ?? $sender->getLevel()->getFolderName();
-		$clonePlot = $this->getPlugin()->getProvider()->getPlot($levelName, (int) $plotIdArray[0], (int) $plotIdArray[1]);
-		$originPlot = $this->getPlugin()->getPlotByPosition($sender);
-		if($originPlot === null) {
+		$selectedPlot = $this->getPlugin()->getProvider()->getPlot($levelName, (int) $plotIdArray[0], (int) $plotIdArray[1]);
+		$standingPlot = $this->getPlugin()->getPlotByPosition($sender);
+		if($standingPlot === null) {
 			$sender->sendMessage(TextFormat::RED . $this->translateString("notinplot"));
 			return true;
 		}
-		if($originPlot->owner !== $sender->getName() and !$sender->hasPermission("myplot.admin.clone")) {
+		if($standingPlot->owner !== $sender->getName() and !$sender->hasPermission("myplot.admin.clone")) {
 			$sender->sendMessage(TextFormat::RED . $this->translateString("notowner"));
 			return true;
 		}
-		if($clonePlot->owner !== $sender->getName() and !$sender->hasPermission("myplot.admin.clone")) {
+		if($selectedPlot->owner !== $sender->getName() and !$sender->hasPermission("myplot.admin.clone")) {
 			$sender->sendMessage(TextFormat::RED . $this->translateString("notowner"));
 			return true;
 		}
-		$plotLevel = $this->getPlugin()->getLevelSettings($originPlot->levelName);
+		$plotLevel = $this->getPlugin()->getLevelSettings($standingPlot->levelName);
 		$economy = $this->getPlugin()->getEconomyProvider();
 		if($economy !== null and !$economy->reduceMoney($sender, $plotLevel->clonePrice)) {
 			$sender->sendMessage(TextFormat::RED . $this->translateString("clone.nomoney"));
 			return true;
 		}
-		if($this->getPlugin()->clonePlot($originPlot, $clonePlot)) {
-			$sender->sendMessage($this->translateString("clone.success"));
+		if($this->getPlugin()->clonePlot($selectedPlot, $standingPlot)) {
+			$sender->sendMessage($this->translateString("clone.success", [$selectedPlot->__toString(), $standingPlot->__toString()]));
 		}else{
 			$sender->sendMessage(TextFormat::RED . $this->translateString("error"));
 		}
