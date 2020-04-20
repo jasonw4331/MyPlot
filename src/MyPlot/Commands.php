547f723f905d9d23b0fe3f14608883009ecc1a41
DerDevHD <derdevhd@gmail.com>
2020-04-20 18:06:47 +0200

    Added FindSubCommand

--- src/MyPlot/Commands.php
+++ src/MyPlot/Commands.php
@@ -11,6 +11,7 @@ use MyPlot\subcommand\ClearSubCommand;
 use MyPlot\subcommand\CloneSubCommand;
 use MyPlot\subcommand\DenyPlayerSubCommand;
 use MyPlot\subcommand\DisposeSubCommand;
+use MyPlot\subcommand\FindSubCommand;
 use MyPlot\subcommand\GenerateSubCommand;
 use MyPlot\subcommand\GiveSubCommand;
 use MyPlot\subcommand\HelpSubCommand;
@@ -75,6 +76,7 @@ class Commands extends PluginCommand
 		$this->loadSubCommand(new SetOwnerSubCommand($plugin, "setowner"));
 		$this->loadSubCommand(new ListSubCommand($plugin, "list"));
 		$this->loadSubCommand(new PvpSubCommand($plugin, "pvp"));
+		$this->loadSubCommand(new FindSubCommand($plugin, "find"));
 		$styler = $this->getPlugin()->getServer()->getPluginManager()->getPlugin("WorldStyler");
 		if($styler !== null) {
 			$this->loadSubCommand(new CloneSubCommand($plugin, "clone"));
