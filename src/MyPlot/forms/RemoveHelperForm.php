8124172a9bf508a46ec5e32b8a87e4ce9f7b747b
jasonwynn10 <jasonwynn10@me.com>
2020-04-21 16:17:41 -0400

    Added RemoveHelper Form

---
+++ src/MyPlot/forms/RemoveHelperForm.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+namespace MyPlot\forms;
+
+
+use MyPlot\MyPlot;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class RemoveHelperForm extends ComplexMyPlotForm {
+	public function __construct() {
+		$plugin = MyPlot::getInstance();
+		$this->setTitle($plugin->getLanguage()->translateString("form.header", [TextFormat::AQUA."Add Helper Form"]));
+		$this->addDropdown(
+			$plugin->getLanguage()->translateString("removehelper.dropdown", [TextFormat::WHITE."Helper Name"]),
+			$this->plot->helpers
+		);
+
+		parent::__construct($plugin, function(Player $player, string $data) use ($plugin) {
+			if(is_null($data)) {
+				$player->getServer()->dispatchCommand($player, $this->plugin->getLanguage()->get("command.name"), true);
+				return;
+			}
+			$player->getServer()->dispatchCommand($player, $this->plugin->getLanguage()->get("command.name")." ".$this->plugin->getLanguage()->get("removehelper.name")." \"$data\"", true);
+		});
+	}
+
+	public function processData(&$data) : void {
+		if(is_null($data))
+			return;
+		var_dump($data);
+		// TODO: convert dropdown return value to player name
+		$data = "player Name";
+		//throw new FormValidationException("Unexpected form data returned");
+	}
+}
--- src/MyPlot/subcommand/RemoveHelperSubCommand.php
+++ src/MyPlot/subcommand/RemoveHelperSubCommand.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 namespace MyPlot\subcommand;
 
+use MyPlot\forms\MyPlotForm;
+use MyPlot\forms\RemoveHelperForm;
 use pocketmine\command\CommandSender;
 use pocketmine\OfflinePlayer;
 use pocketmine\Player;
@@ -47,5 +49,9 @@ class RemoveHelperSubCommand extends SubCommand
 			$sender->sendMessage(TextFormat::RED . $this->translateString("error"));
 		}
 		return true;
+	}
+
+	public function getForm() : MyPlotForm {
+		return new RemoveHelperForm();
 	}
 }
