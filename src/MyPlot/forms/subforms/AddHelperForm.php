c3b0d2cfce80e859bc76286aa3564becd8c3eaae
jasonwynn10 <jasonwynn10@me.com>
2020-05-07 21:31:21 -0400

    Revert crash-fix for helper and kick commands

--- src/MyPlot/forms/subforms/AddHelperForm.php
+++ src/MyPlot/forms/subforms/AddHelperForm.php
@@ -38,12 +38,9 @@ class AddHelperForm extends ComplexMyPlotForm {
 	public function processData(&$data) : void {
 		if(is_null($data))
 			return;
-		elseif(is_array($data)) {
-			$key = array_search($data[0], $this->players);
-			if($key === false)
-				throw new FormValidationException("Invalid form data returned");
-			$data = $this->players[$key];
-		}else
+		elseif(is_array($data))
+			$data = $this->players[$data[0]];
+		else
 			throw new FormValidationException("Unexpected form data returned");
 	}
 }
