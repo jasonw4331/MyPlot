2b263e30e392ca2bc007c74303e160ca559a208e
jasonwynn10 <jasonwynn10@me.com>
2020-05-07 22:40:17 -0400

    Added Home Form

---
+++ src/MyPlot/forms/subforms/HomeForm.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+namespace MyPlot\forms\subforms;
+
+use MyPlot\forms\SimpleMyPlotForm;
+use MyPlot\MyPlot;
+use MyPlot\Plot;
+use pocketmine\form\FormValidationException;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class HomeForm extends SimpleMyPlotForm {
+	/** @var Plot[] $plots */
+	private $plots = [];
+	public function __construct(Player $player) {
+		parent::__construct(null);
+		$plugin = MyPlot::getInstance();
+		$this->setTitle($plugin->getLanguage()->translateString("form.header", [TextFormat::DARK_BLUE."Home Form"]));
+
+		$this->plots = $plugin->getPlotsOfPlayer($player->getName(), $player->getLevel()->getFolderName());
+		$i = 1;
+		foreach($this->plots as $plot) {
+			$this->addButton(TextFormat::DARK_GREEN.$i++.") ".TextFormat::BLUE.$plot->name." ".(string)$plot);
+		}
+
+		$this->setCallable(function(Player $player, ?int $data) use ($plugin) {
+			if(is_null($data)) {
+				$player->getServer()->dispatchCommand($player, $plugin->getLanguage()->get("command.name"), true);
+				return;
+			}
+			$player->getServer()->dispatchCommand($player, $plugin->getLanguage()->get("command.name")." ".$plugin->getLanguage()->get("home.name")." \"$data\"", true);
+		});
+	}
+
+	public function processData(&$data) : void {
+		if(is_null($data))
+			return;
+		elseif(is_int($data))
+			$data += 1;
+		else
+			throw new FormValidationException("Unexpected form data returned");
+	}
+}
--- src/MyPlot/subcommand/HomeSubCommand.php
+++ src/MyPlot/subcommand/HomeSubCommand.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 namespace MyPlot\subcommand;
 
+use MyPlot\forms\MyPlotForm;
+use MyPlot\forms\subforms\HomeForm;
 use MyPlot\Plot;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
@@ -56,5 +58,9 @@ class HomeSubCommand extends SubCommand
 			$sender->sendMessage(TextFormat::RED . $this->translateString("home.error"));
 		}
 		return true;
+	}
+
+	public function getForm(?Player $player = null) : ?MyPlotForm {
+		return new HomeForm($player);
 	}
 }
