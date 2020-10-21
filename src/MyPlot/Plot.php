adeaabe90dede5ac98a0e77dab75f01edec1fb16
HimmelKreis4865 <51092444+HimmelKreis4865@users.noreply.github.com>
2020-10-21 14:52:42 +0200

    Update Plot.php

--- src/MyPlot/Plot.php
+++ src/MyPlot/Plot.php
@@ -155,6 +155,53 @@ class Plot
 		return true;
 	}
 
+		/**
+	 * @api
+	 *
+	 * @return string
+	 */
+	public function getOwner(): string {
+		return $this->owner;
+	}
+
+	/**
+	 * @api
+	 *
+	 * @return int
+	 */
+	public function getX(): int {
+		return $this->X;
+	}
+
+	/**
+	 * @api
+	 *
+	 * @return int
+	 */
+	public function getZ(): int {
+		return $this->Z;
+	}
+
+	/**
+	 * @api
+	 *
+	 * @return string
+	 */
+	public function getLevelName(): string {
+		return $this->levelName;
+	}
+
+	/**
+	 * @api
+	 *
+	 * @param string $username
+	 *
+	 * @return bool
+	 */
+	public function isOwner(string $username) :bool {
+		return ($this->owner === $username);
+	}
+	
 	/**
 	 * @api
 	 *
