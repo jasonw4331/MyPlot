99f8bb5a6eca2051c42a84baf86193b86e5d8840
jasonwynn10 <jasonwynn10@me.com>
2020-12-05 10:18:41 -0500

    Fixed some unnecessary formatting changes

--- src/MyPlot/MyPlot.php
+++ src/MyPlot/MyPlot.php
@@ -1089,9 +1089,9 @@ class MyPlot extends PluginBase
 	/* -------------------------- Non-API part -------------------------- */
 	public function onLoad() : void {
 		if (!\class_exists(SpoonDetector::class)) {
-            $this->getLogger()->critical("SpoonDetector Virion not found! Please re-download MyPlot from Poggit.");
-            return;
-        }
+			$this->getLogger()->critical("SpoonDetector Virion not found! Please re-download MyPlot from Poggit.");
+			return;
+		}
 		$this->getLogger()->debug(TF::BOLD."Loading...");
 		self::$instance = $this;
 		$this->getLogger()->debug(TF::BOLD . "Loading Configs");
@@ -1196,10 +1196,10 @@ class MyPlot extends PluginBase
 		if($this->isDisabled()) {
 			return;
 		}
-		$this->getLogger()->debug(TF::BOLD."Loading Events");
+		$this->getLogger()->debug(TF::BOLD . "Loading Events");
 		$eventListener = new EventListener($this);
 		$this->getServer()->getPluginManager()->registerEvents($eventListener, $this);
-		$this->getLogger()->debug(TF::BOLD."Registering Loaded Levels");
+		$this->getLogger()->debug(TF::BOLD . "Registering Loaded Levels");
 		foreach($this->getServer()->getLevels() as $level) {
 			$eventListener->onLevelLoad(new LevelLoadEvent($level));
 		}
