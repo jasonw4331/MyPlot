234fbf34a23ad7d8aca9a0751da0d1cb02c042a8
supercrafter333 <justcoolstuff99@gmail.com>
2022-01-07 21:20:43 +0100

    Fix bug

--- src/MyPlot/EventListener.php
+++ src/MyPlot/EventListener.php
@@ -12,6 +12,7 @@ use pocketmine\block\BlockLegacyIds;
 use pocketmine\block\BlockLegacyIds as BlockIds;
 use pocketmine\block\Liquid;
 use pocketmine\block\Sapling;
+use pocketmine\block\utils\TreeType;
 use pocketmine\block\VanillaBlocks;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
@@ -217,7 +218,7 @@ class EventListener implements Listener
 				 * so the leaves won't go outside the plot
 				 */
 				$block = $event->getBlock();
-				$maxLengthLeaves = (($block->getMeta() & 0x07) == VanillaBlocks::SPRUCE_SAPLING()->getMeta()) ? 3 : 2;
+				$maxLengthLeaves = (($block->getIdInfo()->getVariant() & 0x07) == TreeType::SPRUCE()) ? 3 : 2;
 				$beginPos = $this->plugin->getPlotPosition($plot);
 				$endPos = clone $beginPos;
 				$beginPos->x += $maxLengthLeaves;
