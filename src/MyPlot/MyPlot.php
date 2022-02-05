48b4a6e502dad9f5ca483a66bc7d838c8b856cd2
jasonwynn10 <jasonwynn10@me.com>
2022-02-04 23:37:05 -0500

    Fixed getPlotBorderingPosition missing its promise resolution

--- src/MyPlot/MyPlot.php
+++ src/MyPlot/MyPlot.php
@@ -463,63 +463,67 @@ class MyPlot extends PluginBase
 	 */
 	public function getPlotBorderingPosition(Position $position) : Promise{
 		$resolver = new PromiseResolver();
-		Await::f2c(function() use ($position){
-			if(!$position->isValid())
-				return null;
-			foreach(Facing::HORIZONTAL as $i){
-				$pos = $position->getSide($i);
-				$x = $pos->x;
-				$z = $pos->z;
-				$levelName = $pos->getWorld()->getFolderName();
-
-				if(!$this->isLevelLoaded($levelName))
+		Await::f2c(
+			function() use ($position){
+				if(!$position->isValid())
 					return null;
+				foreach(Facing::HORIZONTAL as $i){
+					$pos = $position->getSide($i);
+					$x = $pos->x;
+					$z = $pos->z;
+					$levelName = $pos->getWorld()->getFolderName();
 
-				$plotLevel = $this->getLevelSettings($levelName);
-				$plotSize = $plotLevel->plotSize;
-				$roadWidth = $plotLevel->roadWidth;
-				$totalSize = $plotSize + $roadWidth;
-				if($x >= 0){
-					$X = (int) floor($x / $totalSize);
-					$difX = $x % $totalSize;
-				}else{
-					$X = (int) ceil(($x - $plotSize + 1) / $totalSize);
-					$difX = abs(($x - $plotSize + 1) % $totalSize);
-				}
-				if($z >= 0){
-					$Z = (int) floor($z / $totalSize);
-					$difZ = $z % $totalSize;
-				}else{
-					$Z = (int) ceil(($z - $plotSize + 1) / $totalSize);
-					$difZ = abs(($z - $plotSize + 1) % $totalSize);
-				}
-				if(($difX > $plotSize - 1) or ($difZ > $plotSize - 1)){
-					$basePlot = yield $this->dataProvider->getPlot($levelName, $x, $z);
-					if(!$basePlot->isMerged())
+					if(!$this->isLevelLoaded($levelName))
 						return null;
 
-					// no plot found at current location yet, so search cardinal directions
-					$plotN = $basePlot->getSide(Facing::NORTH);
-					if($plotN->isSame($basePlot))
-						return yield $this->dataProvider->getMergeOrigin($plotN);
+					$plotLevel = $this->getLevelSettings($levelName);
+					$plotSize = $plotLevel->plotSize;
+					$roadWidth = $plotLevel->roadWidth;
+					$totalSize = $plotSize + $roadWidth;
+					if($x >= 0){
+						$X = (int) floor($x / $totalSize);
+						$difX = $x % $totalSize;
+					}else{
+						$X = (int) ceil(($x - $plotSize + 1) / $totalSize);
+						$difX = abs(($x - $plotSize + 1) % $totalSize);
+					}
+					if($z >= 0){
+						$Z = (int) floor($z / $totalSize);
+						$difZ = $z % $totalSize;
+					}else{
+						$Z = (int) ceil(($z - $plotSize + 1) / $totalSize);
+						$difZ = abs(($z - $plotSize + 1) % $totalSize);
+					}
+					if(($difX > $plotSize - 1) or ($difZ > $plotSize - 1)){
+						$basePlot = yield $this->dataProvider->getPlot($levelName, $x, $z);
+						if(!$basePlot->isMerged())
+							return null;
 
-					$plotS = $basePlot->getSide(Facing::SOUTH);
-					if($plotS->isSame($basePlot))
-						return yield $this->dataProvider->getMergeOrigin($plotS);
+						// no plot found at current location yet, so search cardinal directions
+						$plotN = $basePlot->getSide(Facing::NORTH);
+						if($plotN->isSame($basePlot))
+							return yield $this->dataProvider->getMergeOrigin($plotN);
 
-					$plotE = $basePlot->getSide(Facing::EAST);
-					if($plotE->isSame($basePlot))
-						return yield $this->dataProvider->getMergeOrigin($plotE);
+						$plotS = $basePlot->getSide(Facing::SOUTH);
+						if($plotS->isSame($basePlot))
+							return yield $this->dataProvider->getMergeOrigin($plotS);
 
-					$plotW = $basePlot->getSide(Facing::WEST);
-					if($plotW->isSame($basePlot))
-						return yield $this->dataProvider->getMergeOrigin($plotW);
-					continue;
+						$plotE = $basePlot->getSide(Facing::EAST);
+						if($plotE->isSame($basePlot))
+							return yield $this->dataProvider->getMergeOrigin($plotE);
+
+						$plotW = $basePlot->getSide(Facing::WEST);
+						if($plotW->isSame($basePlot))
+							return yield $this->dataProvider->getMergeOrigin($plotW);
+						continue;
+					}
+					return yield $this->dataProvider->getPlot($levelName, $X, $Z);
 				}
-				return yield $this->dataProvider->getPlot($levelName, $X, $Z);
-			}
-			return null;
-		});
+				return null;
+			},
+			fn(?Plot $plot) => $resolver->resolve($plot),
+			fn(\Throwable $e) => $resolver->reject()
+		);
 		return $resolver->getPromise();
 	}
 
